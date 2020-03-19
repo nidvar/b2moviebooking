b140fe5a805e4d2aa2290d9747d4f5e18a292f35
Jay Kim <djayshine87@hotmail.com>
2020-03-19 20:31:31 +1100

    seating function working

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
+const alphabet = 'abcdefghijklmnopqr'
+const rowArray = alphabet.toUpperCase().split('')
 //create row
+let result = ''
 const createRow = (row_number)=>{
     for(let i=0; i<=22; i++){
         const row = document.createElement('div');
@@ -11,15 +14,30 @@ const createRow = (row_number)=>{
         }
         row.setAttribute('id', `${row_number}${i}`)
         row.addEventListener('click',(e)=>{
-
-            const x = e.target.id.slice(1)
-            console.log(x)
+            
+            if(row_number<10){
+                const x = e.target.id.slice(1)
+                const y = rowArray[row_number]+x
+                console.log(y)
+                console.log(result)
+                result = result +' - '+ y
+                console.log(result)
+            }else{
+                const x = e.target.id.slice(2)
+                const y = rowArray[row_number]+x
+                result = result +' - '+ y
+            }
+            document.getElementById('results').innerHTML = result
             e.target.classList.toggle('yourSeat')
         })
-        const rows = document.getElementsByClassName('row')
+        const rows = document.getElementsByClassName('rowLetter')
         rows[row_number].appendChild(row)
     }
 }
 
-createRow(0);
-createRow(1);
+for(let i=0; i<=17; i++){
+    const rowLetter = document.createElement('div');
+    rowLetter.classList.add('rowLetter')
+    document.getElementById('cinema').appendChild(rowLetter)
+    createRow(i)
+}
